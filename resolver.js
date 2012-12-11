5ec1c231941f333acd2494b4af5c6decb7204dfc
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2012-12-11 13:17:11 +0100

    new parameter localSearchParam and Tests complete

--- resolver.js
+++ resolver.js
@@ -20,13 +20,35 @@ function resolve(parsedRequest, response,http) {
 			resolveUrl = "http://dispatch.opac.d-nb.de/DB=1.1/CMD?ACT=SRCHA&IKT=8506&SRT=LST_ty&TRM="+parsedRequest.query['zdb'];
 		}
 		// found holding
+		else if(typeof parsedRequest.query['localSearchParam'] == 'undefined') // default search for zdbid
+		{
+			resolveUrl = parsedRequest.query['localUrl'] + parsedRequest.query['zdb'];
+		}
 		else
 		{
-			var arrSigSo = sigSo.split(" / "); // splits Signatur from Standort
-			var sig = arrSigSo[0];
-			//console.log(sig);
-			resolveUrl = parsedRequest.query['localUrl'] + sig;			
+			switch(parsedRequest.query['localSearchParam']){
+				case 'sig': var arrSigSo = sigSo.split(" / "); // splits Signatur from Standort
+							var sig = arrSigSo[0];
+							//console.log(sig);
+							resolveUrl = parsedRequest.query['localUrl'] + sig;
+				break;
+				case 'so': var arrSigSo = sigSo.split(" / "); // splits Signatur from Standort
+							if(typeof  arrSigSo[1] != 'undefined')
+							{
+								var sig = arrSigSo[1];
+							}
+							else
+							{
+								var sig = arrSigSo[0]; // fallback to signatur
+							}
+							//console.log(sig);
+							resolveUrl = parsedRequest.query['localUrl'] + sig;
+				break;
+				default: resolveUrl = parsedRequest.query['localUrl']  + parsedRequest.query['zdb'];
+			}
 		}
+		
+		
 		body = '<html><head><title>ZDB-Resolver</title>'+
 		'<meta http-equiv="refresh" content="0; URL='+resolveUrl+'">'+
 		'</head></html>';
--- router.js
+++ router.js
@@ -1,26 +1,33 @@
 function route(parsedRequest, response,resolve,http) {
-  var errmsg = "";
-  if (typeof parsedRequest.query["localUrl"] == 'undefined' || parsedRequest.query["localUrl"] == "") 
-  {
-	errmsg = "Unable to process request. Parameter 'localUrl' is not set. ";	
-  }
- 
-  if(typeof parsedRequest.query["bib"] == 'undefined' || parsedRequest.query["bib"] == "") 
-  {
-    errmsg = errmsg + "Unable to process request. Parameter 'bib' is not set.";
-  }
-  
-  if(typeof parsedRequest.query["zdb"] == 'undefined' || parsedRequest.query["zdb"] == "") 
-  {
-    errmsg = errmsg + "Unable to process request. Parameter 'zdb' is not set.";
-  }
-  if(errmsg != ""){
-	  response.writeHead(400, {"Content-Type": "text/html"});
-	  response.write("Router says: " + http.STATUS_CODES[400] + ". " + errmsg);
-	  response.end();	
-  } else {
-	resolve(parsedRequest, response,http);
-  }
+	var errmsg = "";
+	if (typeof parsedRequest.query["localUrl"] == 'undefined' || parsedRequest.query["localUrl"] == "") 
+	{
+		errmsg = "Unable to process request. Parameter 'localUrl' is not set. ";	
+	}
+
+	if(typeof parsedRequest.query["localSearchParam"] != 'undefined' 
+		& parsedRequest.query["localSearchParam"] != 'zdb' 
+		& parsedRequest.query["localSearchParam"] != 'sig'
+		& parsedRequest.query["localSearchParam"] != 'so')
+	{
+		errmsg = "Value of 'localSearchParam' can only be one of 'zdb', 'sig' or 'so'.";
+	}
+	if(typeof parsedRequest.query["bib"] == 'undefined' || parsedRequest.query["bib"] == "") 
+	{
+		errmsg = errmsg + "Unable to process request. Parameter 'bib' is not set.";
+	}
+
+	if(typeof parsedRequest.query["zdb"] == 'undefined' || parsedRequest.query["zdb"] == "") 
+	{
+		errmsg = errmsg + "Unable to process request. Parameter 'zdb' is not set.";
+	}
+	if(errmsg != ""){
+		response.writeHead(400, {"Content-Type": "text/html"});
+		response.write("Router says: " + http.STATUS_CODES[400] + ". " + errmsg);
+		response.end();	
+	} else {
+		resolve(parsedRequest, response,http);
+	}
 }
 
 exports.route = route;
